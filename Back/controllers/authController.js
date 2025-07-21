85a670c77b6978ec2f34c52270cbb46c1b418d28
hossam-28 <hossamahmed2863@gmail.com>
2025-07-21 08:46:22 +0300

    Add Back/controllers/authController.js 1753076780667

---
+++ Back/controllers/authController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+const Cart = require('../models/Cart');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({ name, email, password });
+
+    await Cart.create({ user: user._id, items: [] });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password)))
+      return res.status(401).json({ message: 'Invalid credentials' });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } catch (error) {
+    next(error);
+  }
+};
