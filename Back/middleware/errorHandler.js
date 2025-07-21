f5013f85e486d34464c3aaedd1eaa39b6dbeeebc
hossam-28 <hossamahmed2863@gmail.com>
2025-07-21 08:46:13 +0300

    Add Back/middleware/errorHandler.js 1753076772000

---
+++ Back/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(res.statusCode === 200 ? 500 : res.statusCode).json({
+    message: err.message || 'Server Error'
+  });
+};
+
+module.exports = errorHandler;
